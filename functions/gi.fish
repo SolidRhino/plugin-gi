0d4a7889a99445c472592c830b31618a4c5f6568
Solid Rhino <SolidRhino@users.noreply.github.com>
2021-02-16 19:24:59 +0100

    correct bug with test command

--- functions/gi.fish
+++ functions/gi.fish
@@ -1,5 +1,5 @@
 function gi -d "gitignore.io cli for fish"
-  if test $argv[1] = 'update-completions'
+  if test "$argv[1]" = 'update-completions'
     __gi_update_completions
     return $status
   end
